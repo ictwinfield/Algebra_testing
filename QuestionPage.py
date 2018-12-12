a14befcb4fd6508ce496339bc8d3153a626395e0
Mark Winfield <ictwinfield@gmail.com>
2018-12-12 21:11:35 +1300

    Add files via upload

---
+++ QuestionPage.py
@@ -0,0 +1,52 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from PIL import Image
+from questions import *
+from sympy import *
+
+class Quests(Gtk.Grid):
+    def __init__(self, parent):
+        self.parent = parent
+        Gtk.Grid.__init__(self)
+        self.problems = Questions()
+        self.solved = 0
+        self.correct = 0
+
+        blank_image = Image.new("RGB", (400, 200), '#ffffff')
+        blank_image.save('blank.png')
+        self.lbl = Gtk.Label("0/0")
+        self.quest_image = Gtk.Image()
+        self.quest_image.set_from_file("blank.png")
+        self.ans = Gtk.Entry()
+        self.hint_btn = Gtk.Button(label='Hint')
+        self.video = Gtk.Label('Video Help')
+        self.submit_btn = Gtk.Button(label='Start')
+
+        self.attach(self.lbl, 0, 0, 2, 1)
+        self.attach(self.quest_image, 0, 1, 2, 1)
+        self.attach(self.ans, 0, 2, 2, 1)
+        self.attach(self.hint_btn, 0, 3, 1, 1)
+        self.attach(self.video, 1, 3, 1, 1)
+        self.attach(self.submit_btn, 0, 4, 2, 1)
+
+        self.submit_btn.connect("clicked", self.submit)
+
+    def submit(self, widget):
+        if self.solved > 0:
+            if (self.ans.get_text() == self.problems.ans[self.solved - 1]):
+                print("Correct")
+            else:
+                print(self.problems.ans[self.solved - 1])
+            self.quest_image.set_from_file(self.problems.images[self.solved])
+            self.ans.set_text("")
+            self.solved += 1
+            self.lbl.set_text(str(self.correct)+ "/" + str(self.solved))
+
+        if self.solved == 0:
+            self.submit_btn.set_label("Submit")
+            self.solved += 1
+            self.quest_image.set_from_file(self.problems.images[0])
+
+
+        # print("Solved = " + str(self.solved))
---
+++ questions.py
@@ -0,0 +1,67 @@
+from sympy import *
+from random import *
+from PIL import Image
+
+x, y = symbols("x,y")
+
+class Factorise():
+    def __init__(self, level, filename):
+        self.level = level
+        self.fn = filename
+        self.quest = ""
+        self.ans   = ""
+
+
+        if self.level == 1:
+            alpha1 = str(randint(1, 10))
+            s1 = '-' if randint(1,2) == 1 else '+'
+            alpha2 = str(randint(1, 10))
+            s2 = '-' if randint(1,2) == 1 else '+'
+            expr = "(x " + s1 + alpha1 + ")*(x " +s2 + alpha2 + ")"
+            self.quest = expand(expr)
+            self.ans   = factor(self.quest)
+        else:
+            alpha1 = str(randint(1, 5))
+            s1 = '-' if randint(1,2) == 1 else '+'
+            alpha2 = str(randint(1, 5))
+            s2 = '-' if randint(1,2) == 1 else '+'
+            beta1 = str(randint(2, 5))
+            beta2 = str(randint(2, 5))
+            expr = "(" + beta1 + "* x " + s1 + alpha1 + ")*(" + beta2 + "* x " +s2 + alpha2 + ")"
+            self.quest = expand(expr)
+            self.ans   = factor(self.quest)
+
+        preview(self.quest, viewer='file', filename=self.fn, euler=False)
+
+        old_im = Image.open(self.fn)
+        old_size = old_im.size
+        # new_size = (old_size[0] + 40, old_size[1] + 40)
+        new_size = (400, 200)
+
+        new_im = Image.new("RGB", new_size, '#ffffff')
+        new_im.paste(old_im, (int((new_size[0]-old_size[0])/2), int((new_size[1]-old_size[1])/2)))
+
+        new_im.save(self.fn)
+
+    def get_question(self):
+        return self.quest
+
+    def get_answer(self):
+        return self.ans
+
+class Questions():
+    questions = []
+    ans = []
+    images = []
+    for i in range(5):
+        fn = "quest" + str(i) + ".png"
+        images.append(fn)
+        problem = Factorise(2, fn)
+        questions.append(problem.get_question())
+        ans.append(problem.get_answer())
+        # questions.append(Factorise(2, fn).get_question())
+    # for i in range(5):
+    #     print(questions[i].get_question())
+    #     print(questions[i].get_answer())
+    # print(questions[3])
+    # print(ans[3])
