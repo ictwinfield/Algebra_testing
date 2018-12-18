8d0cde2925a6075421f0dc53de3e2b0bce1d7181
Mark Winfield <ictwinfield@gmail.com>
2018-12-18 19:26:42 +1300

    Added review page

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from QuestionPage import Quests
+from ResultsPage import Results_Page
 
 
 class Tester(Gtk.Assistant):
@@ -12,6 +13,7 @@ class Tester(Gtk.Assistant):
 
         self.intro = Gtk.TextView()
         self.quests = Quests(self)
+        self.review = Results_Page(self)
 
         # Set up TextView
         self.intro.set_editable(False)
@@ -27,13 +29,17 @@ class Tester(Gtk.Assistant):
         # Add the pages
         self.append_page(self.intro)
         self.append_page(self.quests)
+        self.append_page(self.review)
 
         self.set_page_type(self.intro, Gtk.AssistantPageType(1))
         self.set_page_title(self.intro, "Introduction")
         self.set_page_complete(self.intro, True)
 
-        self.set_page_type(self.quests, Gtk.AssistantPageType(3))
+        self.set_page_type(self.quests, Gtk.AssistantPageType(4))
         self.set_page_title(self.quests, "Questions")
+
+        self.set_page_type(self.review, Gtk.AssistantPageType(1))
+        self.set_page_title(self.review, "Your Results")
 
         self.connect("cancel", Gtk.main_quit)
         self.connect("close", Gtk.main_quit)
@@ -41,6 +47,7 @@ class Tester(Gtk.Assistant):
 
         self.intro.show()
         self.quests.show()
+        self.review.show()
 
 ass = Tester()
 ass.connect("destroy", Gtk.main_quit)
